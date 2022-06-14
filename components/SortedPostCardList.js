4512994d108e3a2ce34d283c13c4509bc21e1010
mariam-mimi <mariam.kisto.06@icloud.com>
2022-06-14 19:56:59 -0400

    commit SortedPostCardList

---
+++ components/SortedPostCardList.js
@@ -0,0 +1,11 @@
+import PostCard from '../Idealize/components/PostCard'
+import PostCardList from '../Idealize/components/PostCardList'
+import SortBy from '../Idealize/components/SortBy'
+
+export default function SortedPostCardList() {
+    return (
+        <li>
+            <SortBy />
+        </li>
+    )
+}
