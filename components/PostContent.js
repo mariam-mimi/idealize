3b66981b4b044356847870e764a7df7732c09d00
mariam-mimi <mariam.kisto.06@icloud.com>
2022-06-15 19:56:35 -0400

    commit PostContent

---
+++ components/PostContent.js
@@ -0,0 +1,13 @@
+import PostFooter from '../Idealize/components/PostFooter'
+import PostDescription from '../Idealize/components/PostDescription'
+import PostHeading from '../idealize/componenets/PostHeading'
+
+export default function PostContent() {
+    return (
+        <div>
+            <PostHeading />
+            <PostDescription />
+            <PostFooter />
+        </div>
+    )
+}
