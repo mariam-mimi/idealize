ca35a874a75311ab5e66da405b2665ff941de55b
mariam-mimi <mariam.kisto.06@icloud.com>
2022-06-15 19:43:11 -0400

    commit PostDescription

---
+++ components/PostDescription.js
@@ -0,0 +1,23 @@
+import PostFooter from '../Idealize/components/PostFooter'
+
+export default function PostDescription() {
+    return (
+        <><div className="np-card">
+            <div className="post-name-and-upvote post-top">
+                <div className="post-title-and-username">
+
+                    <h1>Title of post</h1>
+
+                    <a href="#">Username123</a>
+                </div>
+                <span className="vote">
+
+                    <img src="../svg/heart-regular.svg" width="22" height="26" />
+
+                    <h3 style="margin-right: 5px;">5</h3>
+                </span>
+            </div>
+        </div>
+        <p className="post-p">This is the content of the post. Bla bla. Ninja Rockets. Once apon a time, a man named Jack ran up a hill. He ran so fast people thought he was a goat. Jack would like to play the violin outside while it was raining. That is very sacreligous. But what was also lamentable was the fact that I can not spell. Yep. So just excuse me pls.</p></>
+    )
+}
