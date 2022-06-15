b96dcbd16e42ae950d403091e40d6160c1f33a52
mariam-mimi <mariam.kisto.06@icloud.com>
2022-06-15 18:33:35 -0400

    commit CommentList

---
+++ components/CommentList.js
@@ -0,0 +1,9 @@
+import CommentCard from "../Idealize/components/CommentCard"
+
+export default function CommentList() {
+    return (
+        <li>
+            <CommentCard />
+        </li>
+    )
+}
