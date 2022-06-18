4dff23651152f10f68eebcb1d293c14066ac7a86
mariam-mimi <mariam.kisto.06@icloud.com>
2022-06-18 13:36:47 -0400

    commit AccountEditForm

---
+++ components/AccountEditForm.js
@@ -0,0 +1,20 @@
+export default function AccountEditForm() {
+    return (
+        <div class="card">
+            <h1>Edit Account</h1>
+            <label for="u_username">Username:</label>
+            <input type="text" id="u_username" name="u_user_name" />
+
+            <label for="u_mail">Email:</label>
+            <input type="email" id="u_mail" name="u_user_email" />
+
+            <label for="change_password">Change Password:</label>
+            <input type="password" id="change_password" name="user_change_password" />
+
+            <label for="u_confirm_password">Confirm Password:</label>
+            <input type="password" id="u_confirm_password" name="u_user_confirm_password" />
+            
+            <button class="save-changes-btn">Save changes</button>
+        </div>
+    )
+}
--- components/Notification.js
+++ components/Notification.js
@@ -1,11 +1,11 @@
 export default function Notification() {
     return (
-        <div class="card">
-            <h1 class="main-line">Notifications</h1>
+        <div className="card">
+            <h1 className="main-line">Notifications</h1>
 
             <div> 
 
-                <div class="noti-message">
+                <div className="noti-message">
                     <p><a href="#">Username123</a> replied to your <a href="#">post</a></p>
                     
                     <p>2/18/22</p>
