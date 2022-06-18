b0a87adadde14f88f7dc71f8856bf10e0e555c99
mariam-mimi <mariam.kisto.06@icloud.com>
2022-06-18 13:48:19 -0400

    commit LogInForm

---
+++ components/LogInForm.js
@@ -0,0 +1,23 @@
+export default function LogInForm() {
+    return (
+        <div>
+            <form method="post" className="login-form">
+                <div>
+                    <label for="mail">Email:</label>
+                    <input type="email" id="mail" name="user_email" />
+
+                    <label for="password">Password:</label>
+                    <input type="password" id="password" name="user_password" />
+
+                    <button type="submit" className="login-btn">Log In</button>
+
+                    <a href="#"><p className="text-center">Forgot password?</p></a>
+
+                    <span className="form-line"></span>
+
+                    <a role="button" href="sign-up.html" className="create-account-btn">Create new account</a>
+                </div>
+            </form>
+        </div>
+    )
+}
