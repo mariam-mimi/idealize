5184754626ec6e5a9e947879e13022e5c1dd6f8c
mariam-mimi <mariam.kisto.06@icloud.com>
2022-06-18 13:05:42 -0400

    commit PostHeader

---
+++ components/PostHeader.js
@@ -0,0 +1,23 @@
+import PostFooter from './PostFooter'
+import PostDescription from './PostDescription'
+
+export default function PostHeader() {
+    return (
+        <div className="np-card">
+            <div className="post-name-and-upvote post-top">
+                <div className="post-title-and-username">
+
+                    <h1>Title of post</h1>
+
+                    <a href="#">Username123</a>
+                </div>
+                <span className="vote">
+
+                    <img src="../svg/heart-regular.svg" width="22" height="26" />
+
+                    <h3 style="margin-right: 5px;">5</h3>
+                </span>
+            </div>
+        </div>
+    )
+}
