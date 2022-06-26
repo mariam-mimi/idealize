cffd771d91282886f22d7eef639a94ba417ffcb4
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2022-06-26 10:05:13 -0400

    Add stories for PostCard and PostCardList

---
+++ stories/components/PostCard.stories.js
@@ -0,0 +1,8 @@
+import PostCardComponent from '../../components/PostCard';
+
+export default {
+    title: 'Posts/PostCard',
+    component: PostCardComponent,
+}
+
+export const PostCard = () => <PostCardComponent />
---
+++ stories/components/PostCardList.stories.js
@@ -0,0 +1,8 @@
+import PostCardListComponent from '../../components/PostCardList';
+
+export default {
+    title: 'Posts/PostCardList',
+    component: PostCardListComponent,
+};
+
+export const PostCardList = () => <PostCardListComponent />;
