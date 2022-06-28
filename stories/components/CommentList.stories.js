cb73f67a74dd236db24832c4408d6d60ae13952d
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2022-06-28 09:09:19 -0400

    Add CommentList and NotificationList stories

---
+++ stories/components/CommentList.stories.js
@@ -0,0 +1,8 @@
+import CommentListComponent from '../../components/CommentList';
+
+export default {
+    title: 'Posts/CommentList',
+    component: CommentListComponent,
+}
+
+export const CommentList = () => <CommentListComponent />
---
+++ stories/components/NotificationList.stories.js
@@ -0,0 +1,8 @@
+import NotificationListComponent from '../../components/NotificationList';
+
+export default {
+    title: 'Layout/NotificationList',
+    component: NotificationListComponent,
+}
+
+export const NotificationList = () => <NotificationListComponent />
