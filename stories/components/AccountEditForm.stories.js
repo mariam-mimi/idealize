869c7b36a73ea6aafc5529d9f5637cb7248436da
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2022-06-28 14:04:59 -0400

    Add form stories for real

---
+++ stories/components/AccountEditForm.stories.js
@@ -0,0 +1,8 @@
+import AccountEditFormComponent from '../../components/AccountEditForm';
+
+export default {
+    title: 'User/AccountEditForm',
+    component: AccountEditFormComponent,
+}
+
+export const AccountEditForm = () => <AccountEditFormComponent />
---
+++ stories/components/LogInForm.stories.js
@@ -0,0 +1,9 @@
+
+import LogInFormComponent from '../../components/LogInForm';
+
+export default {
+    title: 'User/LogInForm',
+    component: LogInFormComponent,
+}
+
+export const LogInForm = () => <LogInFormComponent />
