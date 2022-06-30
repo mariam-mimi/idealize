de3e20a457f466c4daad27f629083557ef8a48bd
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2022-06-30 08:03:03 -0400

    Un-break storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,20 @@
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: [
+    '../stories/**/*.stories.mdx',
+    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-webpack5',
   },
-  "staticDirs": ["../public"]
-}
+  staticDirs: ['../public'],
+  babel: async (options) => ({
+    ...options,
+    plugins: [...options.plugins, 'styled-jsx/babel'],
+  }),
+};
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,12 +1,15 @@
+import '../styles/vars.css';
 import '../styles/main.css';
-import '../styles/log-sign.css'
+import '../styles/buttons.css';
+import '../styles/forms.css';
+// import '../styles/log-sign.css'
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
