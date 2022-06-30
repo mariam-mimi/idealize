12f569406814a2251fbea0452ce0536333c1be17
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2022-06-29 20:26:51 -0400

    Split main stylesheet into several

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
 import Layout from '../components/Layout';
 
+import '../styles/vars.css';
 import '../styles/main.css';
+import '../styles/buttons.css';
+import '../styles/forms.css';
 
 function MyApp({ Component, pageProps }) {
   // Check to see if there is a specific layout we should use, if not use the default one
