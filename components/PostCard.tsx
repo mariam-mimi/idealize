cace8078e1151464e6e4214d23441fa8b9a59c34
mariam-mimi <mariam.kisto.06@icloud.com>
2022-07-06 15:24:11 -0400

    Co-authored-by: zackm2401@protonmail.com <zackm2401@protonmail.com>

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -1,41 +1,51 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-export default function PostCard() {
+export interface Post {
+  id: number;
+  title: string;
+  user: {
+    username: string;
+  };
+  text: string;
+  heartCount: number;
+  isBookmarked: boolean;
+  isHearted: boolean;
+}
+
+interface PropTypes {
+  post: Post;
+}
+
+export default function PostCard({ post }: PropTypes) {
   return (
     <div className="post-card">
       <div className="post-name-and-upvote post-top">
         <div className="post-title-and-username">
-          <h2>Title of post</h2>
+          <h2>{post.title}</h2>
 
-          <a href="#">Username123</a>
+          <a href="#">{post.user.username}</a>
         </div>
         <span className="vote">
           {/* eslint-disable-next-line @next/next/no-img-element */}
           <img
-            src="/svg/heart-regular.svg"
+            src={`/svg/heart-${post.isHearted ? 'solid' : 'regular'}.svg`}
             alt="Heart icon"
             width="20"
             height="26"
           />
 
-          <h3 style={{ marginRight: '5px' }}>5</h3>
+          <h3 style={{ marginRight: '5px' }}>{post.heartCount}</h3>
         </span>
       </div>
-      <p className="post-p">
-        This is the content of the post. Bla bla. Ninja Rockets. Once apon a
-        time, a man named Jack ran up a hill. He ran so fast people thought he
-        was a goat. Jack would like to play the violin outside while it was
-        raining. That is very sacreligous. But what was also lamentable was the
-        fact that I can not spell. Yep. So just excuse me pls.
-      </p>
+      <p className="post-p">{post.text}</p>
       <div className="vfp-bookmark">
-        <Link href="/posts/1">
+        <Link href={`/posts/${post.id}`}>
           <a className="vfp-btn">View full post</a>
         </Link>
         <a className="bookmark" id="book-svg">
           <Image
-            src="/svg/bookmark-regular.svg"
+            src={`/svg/bookmark-${post.isBookmarked ? 'solid' : 'regular'}.svg`}
             alt="Bookmark icon"
             height={24}
             width={18}
--- stories/components/PostCard.stories.js
+++ stories/components/PostCard.stories.js
@@ -5,4 +5,22 @@ export default {
   component: PostCardComponent,
 };
 
-export const PostCard = () => <PostCardComponent />;
+const postData = {
+  id: 1,
+  title: 'Title of post',
+  user: {
+    username: 'Username123',
+  },
+  text: 'This is the content of the post. Bla bla. Ninja Rockets. Once apon a time, a man named Jack ran up a hill. He ran so fast people thought he was a goat. Jack would like to play the violin outside while it was raining. That is very sacreligous. But what was also lamentable was the fact that I can not spell. Yep. So just excuse me pls.',
+  heartCount: 5,
+  isBookmarked: false,
+  isHearted: false,
+};
+
+const Template = (args) => <PostCardComponent {...args} />;
+
+export const PostCard = Template.bind({});
+
+PostCard.args = {
+  post: postData,
+};
