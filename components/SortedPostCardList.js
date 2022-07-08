ebafa1e12f8705b26f0930392f1104f32d8f1e01
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2022-07-08 09:21:16 -0400

    Add post placeholder data for Next.js

--- components/SortedPostCardList.js
+++ components/SortedPostCardList.js
@@ -2,11 +2,47 @@ import PostCard from './PostCard';
 import PostCardList from './PostCardList';
 import SortBy from './SortBy';
 
+const posts = [
+  {
+    id: 1,
+    title: 'Title of post',
+    user: {
+      username: 'Username123',
+    },
+    text: 'This is the content of the post. Bla bla. Ninja Rockets. Once apon a time, a man named Jack ran up a hill. He ran so fast people thought he was a goat. Jack would like to play the violin outside while it was raining. That is very sacreligous. But what was also lamentable was the fact that I can not spell. Yep. So just excuse me pls.',
+    heartCount: 5,
+    isBookmarked: false,
+    isHearted: false,
+  },
+  {
+    id: 2,
+    title: 'Title of post',
+    user: {
+      username: 'Username123',
+    },
+    text: 'This is the content of the post. Bla bla. Ninja Rockets. Once apon a time, a man named Jack ran up a hill. He ran so fast people thought he was a goat. Jack would like to play the violin outside while it was raining. That is very sacreligous. But what was also lamentable was the fact that I can not spell. Yep. So just excuse me pls.',
+    heartCount: 5,
+    isBookmarked: false,
+    isHearted: false,
+  },
+  {
+    id: 3,
+    title: 'Title of post',
+    user: {
+      username: 'Username123',
+    },
+    text: 'This is the content of the post. Bla bla. Ninja Rockets. Once apon a time, a man named Jack ran up a hill. He ran so fast people thought he was a goat. Jack would like to play the violin outside while it was raining. That is very sacreligous. But what was also lamentable was the fact that I can not spell. Yep. So just excuse me pls.',
+    heartCount: 5,
+    isBookmarked: false,
+    isHearted: false,
+  },
+];
+
 export default function SortedPostCardList() {
   return (
     <>
       <SortBy />
-      <PostCardList />
+      <PostCardList posts={posts} />
     </>
   );
 }
--- pages/profile.js
+++ pages/profile.js
@@ -2,11 +2,47 @@ import PostCardList from '../components/PostCardList';
 import UserInfo from '../components/UserInfo';
 import PostVisibilityToggle from '../components/PostVisibilityToggle';
 
+const posts = [
+  {
+    id: 1,
+    title: 'Title of post',
+    user: {
+      username: 'Username123',
+    },
+    text: 'This is the content of the post. Bla bla. Ninja Rockets. Once apon a time, a man named Jack ran up a hill. He ran so fast people thought he was a goat. Jack would like to play the violin outside while it was raining. That is very sacreligous. But what was also lamentable was the fact that I can not spell. Yep. So just excuse me pls.',
+    heartCount: 5,
+    isBookmarked: false,
+    isHearted: false,
+  },
+  {
+    id: 2,
+    title: 'Title of post',
+    user: {
+      username: 'Username123',
+    },
+    text: 'This is the content of the post. Bla bla. Ninja Rockets. Once apon a time, a man named Jack ran up a hill. He ran so fast people thought he was a goat. Jack would like to play the violin outside while it was raining. That is very sacreligous. But what was also lamentable was the fact that I can not spell. Yep. So just excuse me pls.',
+    heartCount: 5,
+    isBookmarked: false,
+    isHearted: false,
+  },
+  {
+    id: 3,
+    title: 'Title of post',
+    user: {
+      username: 'Username123',
+    },
+    text: 'This is the content of the post. Bla bla. Ninja Rockets. Once apon a time, a man named Jack ran up a hill. He ran so fast people thought he was a goat. Jack would like to play the violin outside while it was raining. That is very sacreligous. But what was also lamentable was the fact that I can not spell. Yep. So just excuse me pls.',
+    heartCount: 5,
+    isBookmarked: false,
+    isHearted: false,
+  },
+];
+
 export default function ProfilePage() {
   return (
     <>
       <PostVisibilityToggle />
-      <PostCardList />
+      <PostCardList posts={posts} />
       <UserInfo />
     </>
   );
