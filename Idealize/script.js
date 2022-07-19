efaf156319606de686becc94c70e6c114a645a58
Cal <56606951+Caleb-117@users.noreply.github.com>
2022-07-19 15:10:55 -0400

    Added media queries for create-post.html and notifications.html
    List of all of the changes:
    
    -Validated all HTML files
    -Validated JS file. (still need to fix bug where back to top button doesn't work on first click, but is fine any time after that)
    -Media Queries for notifications.html
    -Media Queries for create-post.html
    -Changes some styles in notifications.html and create-post.html
    -Fixed a style issue with upvote heart number not being aligned in a desirable way

--- Idealize/script.js
+++ Idealize/script.js
@@ -2,58 +2,60 @@
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
 function myFunction() {
+    "use strict";
+    /*jslint browser:true */
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
 // Close the dropdown if the user clicks outside of it
-window.onclick = function(e) {
+window.onclick = function (e) {
+    "use strict";
     if (!e.target.matches('.dropbtn')) {
-    var myDropdown = document.getElementById("myDropdown");
+        var myDropdown = document.getElementById("myDropdown");
         if (myDropdown.classList.contains('show')) {
-        myDropdown.classList.remove('show');
+            myDropdown.classList.remove('show');
         }
     }
-}
+};
 
 // ---------------------------------back to top--------------------------------------
 function backToTop() {
-    const btn = document.querySelector(".back-to-top");
-    const goToTop = () => {
-        document.body.scrollIntoView({
-            behavior: "smooth"
-        });
-    };
+    "use strict";
+    var btn = document.querySelector(".back-to-top");
+    function goToTop() {
+        document.body.scrollIntoView({behavior: "smooth"});
+    }
     btn.addEventListener("click", goToTop);
 }
 
 // ----------------------view-profile.html tabs--------------------------------------
 function clickHandle(evt, userAndSaved) {
-    let i, tabcontent, tablinks;
-  
+    "use strict";
+    var i, tabcontent, tablinks;
     // This is to clear the previous clicked content.
     tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
+    for (i = 0; i < tabcontent.length; i += 1) {
+        tabcontent[i].style.display = "none";
     }
-  
     // Set the tab to be "active".
     tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    for (i = 0; i < tablinks.length; i += 1) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-  
     // Display the clicked tab and set it to active.
     document.getElementById(userAndSaved).style.display = "block";
-    evt.currentTarget.className += " active";
-  }
+    evt.currentTarget.className += "active";
+}
 
 // ------------------------------ reply comment form ---------------------------------
 // shows hidden comment form
 function openForm() {
+    "use strict";
     document.getElementById("myForm").style.display = "flex";
 }
 // closes form
 function closeForm() {
+    "use strict";
     document.getElementById("myForm").style.display = "none";
 }
 
